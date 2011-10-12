11a80c4223e8bf4edaf872f492ffe745ca0d46a7
David Rice <me@davidjrice.co.uk>
2011-10-12 19:59:23 +0100

    Rails 3.1.1 app asset_sync

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+AssetSyncTest311::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AssetSyncTest311::Application.config.secret_token = '701efb211a4eb8f34f86cea2eada5dff02b0d740b66be2549cfec131723ad49f2b147c33a1f3e4ea72eef114634bba7a126b2eac3e59c8efea5733e6186fc22f'
