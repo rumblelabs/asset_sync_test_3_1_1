7148b70b05b5dd2bf9441a6b3f1781b88cb32673
David Rice <me@davidjrice.co.uk>
2011-10-15 15:04:03 +0100

    Use correct bucket

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ AssetSyncTest311::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "http://asset-sync-test-rails-3-1-1.amazonaws.com"
+  config.action_controller.asset_host = "http://asset-sync-test-3-1-1.s3.amazonaws.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
