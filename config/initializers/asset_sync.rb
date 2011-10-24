4b5dff2d80d6cfe1fc0a52bb70e20c02a8ea0eb0
David Rice <me@davidjrice.co.uk>
2011-10-24 23:36:11 +0100

    Update asset_sync test rackspace compatible version

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,10 +1,8 @@
-# I don't get loaded at all. Even with the version of AssetSync with an Initializer defined in the Gem.
-
 AssetSync.configure do |config|
-  config.aws_access_key = ENV['AWS_ACCESS_KEY']
-  config.aws_access_secret = ENV['AWS_ACCESS_SECRET']
-  config.aws_bucket = ENV['AWS_BUCKET']
-  config.aws_region = 'eu-west-1'
+  config.aws_access_key_id = ENV['AWS_ACCESS_KEY']
+  config.aws_secret_access_key = ENV['AWS_ACCESS_SECRET']
+  config.fog_directory = ENV['AWS_BUCKET']
+  config.fog_region = 'eu-west-1'
   config.existing_remote_files = "keep"
-  config.gzip_compression = true
+  # config.gzip_compression = true
 end
