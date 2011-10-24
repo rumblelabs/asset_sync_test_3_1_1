856c37eba04f3e94b889a6b184402fb5967e3d70
David Rice <me@davidjrice.co.uk>
2011-10-24 23:42:07 +0100

    Use fog_provider aws

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,4 +1,5 @@
 AssetSync.configure do |config|
+  config.fog_provider = 'AWS'
   config.aws_access_key_id = ENV['AWS_ACCESS_KEY']
   config.aws_secret_access_key = ENV['AWS_ACCESS_SECRET']
   config.fog_directory = ENV['AWS_BUCKET']
