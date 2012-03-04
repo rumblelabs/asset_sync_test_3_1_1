e70049fc1dd407e90b328389fa5299b3d4e87f9e
David Rice <me@davidjrice.co.uk>
2012-03-04 20:34:05 +0000

    Don't use custom initializer if including asset_sync in only the assets bundle

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,14 +1,14 @@
-AssetSync.configure do |config|
-  config.fog_provider = 'AWS'
-  config.aws_access_key_id = ENV['AWS_ACCESS_KEY']
-  config.aws_secret_access_key = ENV['AWS_ACCESS_SECRET']
-  config.fog_directory = ENV['AWS_BUCKET']
-  config.fog_region = 'eu-west-1'
-  # config.aws_access_key = ENV['AWS_ACCESS_KEY']
-  # config.aws_access_secret = ENV['AWS_ACCESS_SECRET']
-  # config.aws_bucket = ENV['AWS_BUCKET']
-  #config.aws_region = 'eu-west-1'
-  config.existing_remote_files = "keep"
-  config.gzip_compression = true
-  config.manifest = false
-end
+# AssetSync.configure do |config|
+#   config.fog_provider = 'AWS'
+#   config.aws_access_key_id = ENV['AWS_ACCESS_KEY']
+#   config.aws_secret_access_key = ENV['AWS_ACCESS_SECRET']
+#   config.fog_directory = ENV['AWS_BUCKET']
+#   config.fog_region = 'eu-west-1'
+#   # config.aws_access_key = ENV['AWS_ACCESS_KEY']
+#   # config.aws_access_secret = ENV['AWS_ACCESS_SECRET']
+#   # config.aws_bucket = ENV['AWS_BUCKET']
+#   #config.aws_region = 'eu-west-1'
+#   config.existing_remote_files = "keep"
+#   config.gzip_compression = true
+#   config.manifest = false
+# end
